135c98a5e7fdfb82804c4e7576ca44aaaf86b8aa
thukasab <65799121+thukasab@users.noreply.github.com>
2022-03-18 21:40:38 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use script';
+
+{
+    const dts = document.querySelectorAll('dt');
+
+    dts.forEach(dt => {
+        dt.addEventListener('click', () => {
+            dt.parentNode.classList.toggle('appear');
+        });
+    })
+}
